5d84aded4f954c3efa3895dc39f9d4c79d16e65c
throwaway-lamden <71742430+throwaway-lamden@users.noreply.github.com>
2021-03-23 13:47:05 -0400

    More basic functions

--- contracts/basic_vault.py
+++ contracts/basic_vault.py
@@ -20,12 +20,13 @@ def create_vault(vault_type: int, amount_of_dai: float, amount_of_collateral: fl
     cdp_number = cdp[current_value] 
     cdp[current_value] += 1
     
-    cdp[cdp_number, owner] = ctx.caller
+    cdp[cdp_number, "owner"] = ctx.caller
+    cdp[cdp_number, "open"] = True
     
-    cdp[cdp_number, collateral_type] = vault_type
-    cdp[cdp_number, dai] = amount_of_dai
-    cdp[cdp_number, collateral_amount] = amount_of_collateral
-    cdp[cdp_number, time] = now.seconds #TODO: make sure this works
+    cdp[cdp_number, "collateral_type"] = vault_type
+    cdp[cdp_number, "dai"] = amount_of_dai
+    cdp[cdp_number, "collateral_amount"] = amount_of_collateral
+    cdp[cdp_number, "time"] = now.seconds #TODO: make sure this works
     
     importlib.transfer_from(amount=amount_of_collateral, to=ctx.this, main_account=ctx.caller)
     
@@ -38,8 +39,9 @@ def create_vault(vault_type: int, amount_of_dai: float, amount_of_collateral: fl
     return cdp_number
     
 @export
-def close_vault(number: int):
-    assert cdp[cdp_number, owner] == ctx.caller, "Not the owner!"
+def close_vault(cdp_number: int):
+    assert cdp[cdp_number, "owner"] == ctx.caller, "Not the owner!"
+    assert cdp[cdp_number, "open"] is True, "Vault has already been closed!"
     
     stability_ratio = vaults["issued"] / vaults["total"]
     redemption_cost = cdp[number, "amount_of_dai"] * stability_ratio
@@ -53,14 +55,19 @@ def close_vault(number: int):
     vaults[vault_type, "issued"] -= cdp[number, "dai"]
     vaults[vault_type, "total"] -= redemption_cost
     
+    cdp[cdp_number, "open"] = False
+    
     importlib.transfer(amount=cdp[cdp_number, "collateral_amount"], to=ctx.caller)
     
 @export
-def fast_force_close_vault(number: int):
+def fast_force_close_vault(cdp_number: int):
+    assert cdp[cdp_number, "open"] is True, "Vault has already been closed!"
+    
     stability_ratio = vaults["issued"] / vaults["total"]
     redemption_cost_without_fee = cdp[number, "amount_of_dai"] * stability_ratio 
     redemption_cost = redemption_cost_without_fee * 1.1
     fee = redemption_cost * (stability_rate * (now.seconds - cdp[number, time]))
+    redemption_cost += fee
     
     amount_of_collateral = cdp[number, "collateral_amount"]
     collateral_type = cdp[number, "collateral_type"]
@@ -83,7 +90,7 @@ def fast_force_close_vault(number: int):
         vaults[vault_type, "total"] -= redemption_cost
 
     else:
-        redemption_cost, redemption_cost_without_fee = redemption_cost * collateral_percent / 1.03, redemption_cost_without_fee * collateral_percent / 1.03
+        redemption_cost, redemption_cost_without_fee = redemption_cost * (collateral_percent / 1.03), redemption_cost_without_fee * (collateral_percent / 1.03)
         
         dai_contract.transfer_from(amount=redemption_cost + fee, to=ctx.this, main_account=ctx.caller)
         dai_contract.burn(amount=redemption_cost)
@@ -97,12 +104,48 @@ def fast_force_close_vault(number: int):
         
     stability_pool[cdp[number, "collateral_type"]] += redemption_cost - redemption_cost_without_fee
     
-@export
-def open_force_close_auction():
+    return amount
     
 @export
-def bid_on_force_close():
+def open_force_close_auction(cdp_number: int):
+    assert cdp[cdp_number, "open"] is True, "Vault has already been closed!"
+    assert cdp[cdp_number, "auction", "open"] is False, "Auction is already taking place!" # May not work
+    
+    cdp[cdp_number, "open"] = False # This contract may only be bid on, and not closed
+    
+    cdp[cdp_number, "auction", "highest_bidder"] = ctx.caller
+    cdp[cdp_number, "auction", "top_bid"] = 0
+    cdp[cdp_number, "auction", "time"] = now.seconds #TODO: make sure this works
     
 @export
-def change_state():
+def bid_on_force_close(cdp_number: int):
+    assert cdp[cdp_number, "open"] is True, "Vault has already been closed!"
+    assert cdp[cdp_number, "auction", "open"] is False, "Auction is already taking place!" # May not work
+    
+@export
+def sync_stability_pool(vault_type: int):
+    
+@export
+def settle_force_close(cdp_number: int):
+    assert now.seconds - cdp[cdp_number, "auction", "time"] > vaults[vault_type, "minimum_auction_time"], "Auction is still open!"
+    
+    do_settle_stuff_here()
+    
+@export
+def change_state(key: str, new_value: str, convert_to_decimal: bool=False):
+    assert state["OWNER"] == ctx.caller, "Not the owner!"
+    
+    if convert_to_decimal:
+        new_value = decimal(new_value)
+    state[key] = new_value
+        
+    return new_value
+    
+@export
+def change_any_state(key: Any, new_value: Any):
+    assert state["OWNER"] == ctx.caller, "Not the owner!"
+    
+    state[key] = new_value
+        
+    return new_value
     
