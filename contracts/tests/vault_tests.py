829d1c5d9068c03997c606d311d44754f33ab56d
throwaway-lamden <71742430+throwaway-lamden@users.noreply.github.com>
2021-03-30 13:06:50 -0400

    Added test framework

--- contracts/tests/vault_tests.py
+++ contracts/tests/vault_tests.py
@@ -7,19 +7,26 @@ class VaultTests(unittest.TestCase):
     def setUp(self):
         self.client = ContractingClient()
         self.client.flush()
+        
         with open('dai_token.py') as file:
             dai = file.read()
+            
         with open('basic_vault.py') as file:
             vault = file.read()
+            
         with open('currency.py') as file:
             currency = file.read()
+            
         with open('oracle.py') as file:
             oracle = file.read()
+            
         self.client.submit(dai, name='dai_contract', constructor_args={
                            'vk': 'me', 'owner': 'default_owner'})
+        
         self.client.submit(vault, name='vault_contract')
         self.client.submit(currency, name='currency')
         self.client.submit(oracle, name='oracle')
+        
         self.dai = self.client.get_contract('dai_contract')
         self.vault = self.client.get_contract('vault_contract')
         self.currency = self.client.get_contract("currency")
@@ -87,6 +94,23 @@ class VaultTests(unittest.TestCase):
         except AssertionError as message:
             assert 'owner' in str(message)
 
+    def test_change_owner_works(self):
+        self.dex.change_state(key="OWNER", new_value="stu")
+        self.assertEqual(self.dex.state["OWNER"], "stu")
+        
+        self.dex.change_state(key="OWNER", new_value="jeff", signer="stu")
+        self.assertEqual(self.dex.state["OWNER"], "jeff")
+        
+        self.dex.change_state(key="FOO", new_value=1, convert_to_decimal=True, signer="jeff")
+        self.assertEqual(self.dex.state["FOO"], 1)
+        
+    def test_change_owner_twice_fails(self):
+        self.dex.change_state(key="OWNER", new_value="stu")
+        self.assertEqual(self.dex.state["OWNER"], "stu")
+        
+        with self.assertRaises(AssertionError):
+            self.dex.change_state(key="OWNER", new_value="stu")
+            
     def test_state_invalid_type(self):
         try:
             # int is invalid value
@@ -105,3 +129,44 @@ class VaultTests(unittest.TestCase):
         assert self.vault.vaults['testing2'] == 'testing2'
         self.vault.change_state(key='testing2', new_value='again2')
         assert self.vault.vaults['testing2'] == 'again2'
+        
+    def close_vault_works(self):
+        pass
+    
+    def close_vault_closes_vault(self):
+        pass
+    
+    def close_vault_updates_reserves(self):
+        pass
+    
+    def close_vault_takes_dai(self):
+        pass
+    
+    def close_vault_takes_dai_and_stability_fee(self):
+        pass
+    
+    def close_vault_adjusts_based_on_reserves(self): # use ENV
+        pass
+    
+    def close_vault_adjusts_based_on_reserves_and_stability_fee(self): # use ENV
+        pass
+    
+    def close_vault_takes_dai(self):
+        pass
+    
+    def close_vault_returns_collateral(self):
+        pass
+    
+    def close_vault_funds_burned(self):
+        pass
+    
+    def close_vault_fee_not_burned(self):
+        pass
+    
+    def close_vault_unauthorised(self):
+        pass
+    
+    def close_vault_twice_fails(self):
+        pass
+    
+    
