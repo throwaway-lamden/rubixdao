e84006bb189055cbfd955a19a69039d4fcdff73f
throwaway-lamden <71742430+throwaway-lamden@users.noreply.github.com>
2021-04-01 13:05:27 -0400

    Patch tests to work with rename

--- contracts/tests/auction_tests.py
+++ contracts/tests/auction_tests.py
@@ -7,19 +7,21 @@ class AuctionTests(unittest.TestCase):
     def setUp(self):
         self.client = ContractingClient()
         self.client.flush()
-        with open('dai_token.py') as file:
+        with open('dai.py') as file:
             dai = file.read()
-        with open('basic_vault.py') as file:
+        with open('vault.py') as file:
             vault = file.read()
-        with open('currency.py') as file:
+        with open('test_currency.py') as file:
             currency = file.read()
         with open('oracle.py') as file:
             oracle = file.read()
+            
         self.client.submit(dai, name='dai_contract', constructor_args={
                            'owner': 'default_owner'})
         self.client.submit(vault, name='vault_contract')
         self.client.submit(currency, name='currency')
         self.client.submit(oracle, name='oracle')
+        
         self.dai = self.client.get_contract('dai_contract')
         self.vault = self.client.get_contract('vault_contract')
         self.currency = self.client.get_contract('currency')
