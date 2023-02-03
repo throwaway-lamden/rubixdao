640aff91427fd14645709f4906d12de829850b54
throwaway-lamden <71742430+throwaway-lamden@users.noreply.github.com>
2023-02-02 19:24:08 -0500

    Revert "Fix type checking and adding and removing vaults (#9)"
    This reverts commit 5d175a6fe66990976829d79f2be1af492973e476.

--- contracts/vault.py
+++ contracts/vault.py
@@ -342,11 +342,7 @@ def add_vault(collateral_type: str, collateral_amount: float, auction_time: floa
     assert vaults['OWNER'] == ctx.caller, 'Not the owner!'
 
     vault_number = vaults['current_number']
-    
-    tmp_list = vaults['list']
-    tmp_list.append(vault_number)
-    vaults['list'] = tmp_list
-
+    vaults['list'].append(vault_number)
     vaults['current_number'] += 1
 
     vaults[vault_number, 'collateral_type'] = collateral_type
@@ -363,17 +359,14 @@ def add_vault(collateral_type: str, collateral_amount: float, auction_time: floa
 @export
 def remove_vault(vault_type: int):
     assert vaults['OWNER'] == ctx.caller, 'Not the owner!'
-    
-    tmp_list = vaults['list']
-    tmp_list.remove(vault_type)
-    vaults['list'] = tmp_list
+    vaults['list'].remove(vault_type)
 
 
 @export
 def change_state(key: str, new_value: str, convert_to_decimal: bool = False):
     assert vaults['OWNER'] == ctx.caller, 'Not the owner!'
-    assert isinstance(key, str), 'Invalid type for key'
-    assert isinstance(new_value, str), 'Invalid type for new value'
+    assert type(key) == str, 'Invalid type for key'
+    assert type(new_value) == str, 'Invalid type for new value'
 
     if convert_to_decimal:
         new_value = decimal(new_value)
